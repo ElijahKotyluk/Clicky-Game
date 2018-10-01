e4f2a069dcfac430354a5756672201bfe134a5c5
Eli Kotyluk <eli@riboso.me>
2018-10-01 16:04:10 -0700

    Add Header and Wrapper components. Add state storage for score and high score.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from "./components/Header"
+import Wrapper from "./components/Wrapper"
+import './App.css'
 
 class App extends Component {
+
+  state = {
+  score: 0,
+  highscore: 0
+};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Wrapper>
+          <Header>Clicky Game</Header>
+        </Wrapper>
       </div>
     );
   }
